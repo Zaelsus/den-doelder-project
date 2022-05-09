8db800231ce01e42326fffaeedd8f025f05838a0
roth0010 <89872836+roth0010@users.noreply.github.com>
2022-05-09 14:11:09 +0200

    I giveth, and I taketh away

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,17 @@
+<aside class="main-sidebar sidebar-dark-purple elevation-4">
+        <a href="{{ route('home') }}" class="brand-link">
+        <img src="\img\tinygans.png"
+             alt="Goose hehe"
+             class="brand-image img-circle elevation-3">
+        <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
+    </a>
+
+    <div class="sidebar">
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                @include('layouts.menu')
+            </ul>
+        </nav>
+    </div>
+
+</aside>
