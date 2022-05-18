c75bca0ecea386c6525e2778ae15f032d7737d41
roth0010 <89872836+roth0010@users.noreply.github.com>
2022-05-18 14:31:01 +0200

    Added water skiing squirrels

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,15 +4,27 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>404 Custom Error Page Example</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-<div class="container mt-5 pt-5">
-    <div class="alert alert-danger text-center">
-        <h2 class="display-3">404</h2>
-        <p class="display-5">Oops! Something is wrong.</p>
-        <img src="/img/squirrel.gif">
+    <div class="text-center">
+        <div class="bg-blue" style="padding-top: 1rem; padding-bottom: 1rem">
+            <h1>Error: 404</h1>
+            <p>We're looking, we promise!</p>
+            <div>
+                <img
+                    src="/img/squirrel.gif"
+                    style="border-radius: .75rem;
+                    border-width: .5rem;
+                    border-color: black;
+                    border-style: solid"
+                >
+            </div>
+        </div>
+        <div style="padding-top: 1rem; padding-bottom: 1rem">
+            <p>Our elite search and rescue team of water skiing squirrels are searching for the page you requested.</p>
+            <p>Maybe check the URL to help them find the page!</p>
+            <a href="{{ route('home') }}"><button class="btn bg-dark align-center">Back To Home</button></a>
+        </div>
     </div>
-</div>
 </body>
-</html>
