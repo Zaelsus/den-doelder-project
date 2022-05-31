75f3f84e0affc14ebddb78700fc9bfb21da793ae
Manon Mampaey <mamp0002@hz.nl>
2022-05-31 10:12:55 +0200

    changed the notes index page

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    <br>
     <div class="container-fluid">
         <div class="card">
             <div class="card-header bg-gradient-olive">
--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -1,32 +1,47 @@
 @extends('layouts.app')
-
-@section('header')
-    Notes Overview
-@endsection
-
 @section('content')
-    <section class="section">
-        <div class="container">
-            <div class="columns">
-                <button
-                    onclick=window.location.href="{{route('notes.create')}}">
-                    Create Note
-                </button>
-                <div class="column is-full">
-                    <br>
-                        @foreach($notes as $note)
-                        <br>
-                            <details>
-                                <summary>{{$note->title}} - {{$note->created_at}}</summary>
-                            <p>{{$note->content}}</p>
-                                <button
-                                    onclick=window.location.href="{{route('notes.edit', $note)}}">
-                                    Edit Note
-                                </button>
-                            </details>
-                        @endforeach
-                </div>
+    <br>
+    <div>
+        <div class="card">
+            <div class="card-header bg-gradient-olive">
+                <h3 class="text-center">Overview Notes</h3>
             </div>
+            <div class="card-body">
+                <table class="table table-bordered table-hover">
+                    <thead class="bg-gradient-olive opacity-70">
+                    <tr>
+                        <th scope="col">Order Number</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Content</th>
+                        <th scope="col">Created At</th>
+                        <th scope="col"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($notes as $note)
+                        <tr>
+                            <td>{{ $note->order->order_number}}</td>
+                            <td>{{ $note->title}}</td>
+                            <td style="width: 30%">
+                                <details>
+                                    <summary>{{ substr($note->content, 0, 40) }}</summary>
+                                    <p>{{ substr($note->content, 40) }}</p>
+                                </details>
+                            </td>
+                            <td>{{ $note->created_at }}</td>
+                            <td><button onclick=window.location.href="{{route('notes.edit', $note)}}">
+                                    Edit Note
+                                </button></td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                <br>
+                    <button class="btn btn-lg bg-gradient-olive opacity-70"
+                        onclick=window.location.href="{{route('notes.create')}}">
+                        Create Note
+                    </button>
+                </div>
         </div>
-    </section>
+    </div>
 @endsection
