215cc25d72ae166f53c2b7f08c1ec1c08a9058bd
roth0010 <89872836+roth0010@users.noreply.github.com>
2022-06-03 14:55:04 +0200

    Removed old code from Drawings

--- resources/views/pallets/show.blade.php
+++ resources/views/pallets/show.blade.php
@@ -98,7 +98,7 @@
                     <div class="d-grid gap-2 d-md-flex justify-content-md-end">
                         <button type="button" class="btn colour-orange" data-toggle="modal"
                                 data-target="#exampleModalCenter">
-                            Details
+                            More Details
                         </button>
                     </div>
                 </div>
@@ -115,68 +115,6 @@
                     width="100%"
                     height="auto"
                 >
-            </div>
-        </div>
-        <div class="flex-container">
-            <div class="flex-item-left">
-                <h3><span
-                        class="badge colour-orange align-content-lg-stretch justify-content-center">Pallet Details</span>
-                </h3>
-                <ul style="padding: 0; margin: 0;">
-                    <li class="list_no_bullets" style="list-style: none">
-                        <strong>Pallet Number:</strong>
-                        {{ $pallet->pallet_number }}
-                    </li>
-                    <li class="list_no_bullets" style="list-style: none">
-                        <strong>Dimensions:</strong>
-                        {{ $pallet->measurements }}
-                    </li>
-                    <li class="list_no_bullets" style="list-style: none">
-                        <strong>Classification:</strong>
-                        {{ $pallet->classification }}
-                    </li>
-                    <li class="list_no_bullets" style="list-style: none">
-                        <strong>Treatments:</strong>
-                        {{ $pallet->treatments }}
-                    </li>
-                    <li class="list_no_bullets" style="list-style: none">
-                        <strong>Materials:</strong>
-                        {{ $pallet->details_materialen }}
-                    </li>
-                    <li class="list_no_bullets" style="list-style: none">
-                        <strong>Wood Details:</strong>
-                        {{ $pallet->details_nieuw_hout }}
-                    </li>
-                    <br>
-                </ul>
-                <h3><span
-                        class="badge colour-orange align-content-lg-stretch justify-content-center">Specifications</span>
-                </h3>
-                <p><strong>Order-specific Notes:</strong>
-                    {{ $pallet->details_specifieke_bladnotities }}</p>
-            </div>
-            <div class="flex-item-right">
-                <h3><span class="badge colour-orange align-content-lg-stretch justify-content-center">Componenten</span>
-                </h3>
-                <ul style="padding: 0; margin: 0">
-                    <li class="list_no_bullets" style="list-style: none">
-                        <p class="no-margin-bottom"><strong>Bovendek:</strong></p>
-                        <p class="no-margin-bottom">{{ $pallet->details_components_bovendek }}</p>
-                    </li>
-                    <li class="list_no_bullets" style="list-style: none">
-                        <p class="no-margin-bottom"><strong>Onderdek:</strong></p>
-                        <p class="no-margin-bottom">{{ $pallet->details_components_onderdek }}</p>
-                    </li>
-                    <li class="list_no_bullets" style="list-style: none">
-                        <p class="no-margin-bottom"><strong>Boventussendek:</strong></p>
-                        <p class="no-margin-bottom">{{ $pallet->details_components_boventussendek }}</p>
-                    </li>
-                    <li class="list_no_bullets" style="list-style: none">
-                        <p class="no-margin-bottom"><strong>Klossen:</strong></p>
-                        <p class="no-margin-bottom">{{ $pallet->details_components_klossen }}</p>
-                    </li>
-                    <br>
-                </ul>
             </div>
         </div>
     </div>
