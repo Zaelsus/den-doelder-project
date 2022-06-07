e026772d300d71cd9e4e7d311f49a3608810062a
Dejihz <olag0002@hz.nl>
2022-06-07 18:38:45 +0200

    Update editquantity.blade.php

--- resources/views/orders/editquantity.blade.php
+++ resources/views/orders/editquantity.blade.php
@@ -9,13 +9,13 @@
                         @csrf
                         @method('PUT')
                         <div class="card"> {{-- The form is placed inside a Bulma Card component --}}
-                            <header class="card-header">
+                            <header class="card-header bg-secondary">
                                 <p class="card-header-title"> {{-- The Card header content --}}
                                     Edit the pallet log {{$order->order_number . ' of customer ' . $order->client_name}}
                                 </p>
                             </header>
 
-                            <div class="card-content">
+                            <div class="card-content table table-bordered table-hover table-light ">
                                 <div>
                                     Quantity Ordered: {{$order->quantity_production}}
                                 </div>
