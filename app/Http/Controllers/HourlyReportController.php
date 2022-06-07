abe74108747b8ec4c83145e34b21b0d37dc4818e
NickGMeade <90179901+NickGMeade@users.noreply.github.com>
2022-06-07 16:39:58 +0200

    Update HourlyReportController.php
    Added TODO note to Edit method

--- app/Http/Controllers/HourlyReportController.php
+++ app/Http/Controllers/HourlyReportController.php
@@ -63,6 +63,7 @@ class HourlyReportController extends Controller
      */
     public function edit(HourlyReport $hourlyReport)
     {
+        // TOFIX need to have the Edit blade correctly display saved data
         return view('hourlyReports.edit', compact('hourlyReport'));
     }
 
