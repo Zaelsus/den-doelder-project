515958e0975f66db71c8ac0095b9961255dd6aa9
Shirel Snopik <snop0001@hz.nl>
2022-06-22 15:32:16 +0200

    pressed enter

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -107,15 +107,6 @@
         </section>
     </div>
 
-    {{--    Removed, but code saved if we need it later--}}
-    {{--    <!-- Main Footer -->--}}
-    {{--    <footer class="main-footer bg-blue">--}}
-    {{--        <div class="float-right d-none d-sm-block">--}}
-    {{--            <b>Version</b> 3.0.5--}}
-    {{--        </div>--}}
-    {{--        <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights--}}
-    {{--        reserved.--}}
-    {{--    </footer>--}}
 </div>
 
 <script src="{{ mix('js/app.js') }}" defer></script>
